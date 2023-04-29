5b50459d54de8bac7bd28476910f89a29027e9f0
srajankumar <kumarsrajan02@gmail.com>
2023-04-29 15:58:23 +0530

    navigation bar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,12 +2,7 @@ import Image from "next/image";
 import Head from "next/head";
 import Script from "next/script";
 
-import Link from "next/link";
-
 import styles from "../styles/home.module.css";
-
-import styles1 from "../styles/home1.module.css";
-import styles2 from "../styles/home2.module.css";
 
 import { Inter } from "next/font/google";
 
@@ -21,141 +16,47 @@ export default function Home() {
 
       <Script src="/hello.js"></Script>
 
-      <nav className={styles.mainnav}>
-        <ul>
-          <Link href="/">
-            <li>Home</li>
-          </Link>
-          <Link href="/about">
-            <li>About</li>
-          </Link>
-          <Link href="/blog">
-            <li>Blog</li>
-          </Link>
-          <Link href="/contact">
-            <li>Contact</li>
-          </Link>
-        </ul>
-      </nav>
-
       <div className="flex flex-col place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
         <div className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] ">
-          <h1 className="font-mono font-bold">Hunting Coder</h1>
-          <p>A blog for coders by a coder</p>
-        </div>
-        <div>
-          <h2>Popular Blogs</h2>
-          <div className="blogItem">
-            <h3>How to learn javascript in 2022</h3>
-            <p>
-              JavaSript is the language used to design the logic for the web
-            </p>
-          </div>
-          <div className="blogItem">
-            <h3>How to learn javascript in 2022</h3>
-            <p>
-              JavaSript is the language used to design the logic for the web
-            </p>
-          </div>
-          <div className="blogItem">
-            <h3>How to learn javascript in 2022</h3>
-            <p>
-              JavaSript is the language used to design the logic for the web
-            </p>
-          </div>
-          <div className="blogItem">
-            <h3>How to learn javascript in 2022</h3>
-            <p>
-              JavaSript is the language used to design the logic for the web
-            </p>
-          </div>
-          <div className="blogItem">
-            <h3>How to learn javascript in 2022</h3>
-            <p>
-              JavaSript is the language used to design the logic for the web
-            </p>
+          <div className={styles.headermain}>
+            <h1 className=" font-mono font-bold">Hunting Coder</h1>
+            <p>A blog for hunting coders by a hunting coder</p>
           </div>
         </div>
-        <div>
-          <Image
-            src="/vercel.svg"
-            alt="Vercel Logo"
-            className="dark:invert"
-            width={100}
-            height={24}
-          />
+        <div className={styles.blogPage}>
+          <h2 className="font-bold">Popular Blogs</h2>
+          <div className="blogItem">
+            <h3>How to learn javascript in 2022</h3>
+            <p>
+              JavaSript is the language used to design the logic for the web
+            </p>
+          </div>
+          <div className="blogItem">
+            <h3>How to learn javascript in 2022</h3>
+            <p>
+              JavaSript is the language used to design the logic for the web
+            </p>
+          </div>
+          <div className="blogItem">
+            <h3>How to learn javascript in 2022</h3>
+            <p>
+              JavaSript is the language used to design the logic for the web
+            </p>
+          </div>
+          <div className="blogItem">
+            <h3>How to learn javascript in 2022</h3>
+            <p>
+              JavaSript is the language used to design the logic for the web
+            </p>
+          </div>
+          <div className="blogItem">
+            <h3>How to learn javascript in 2022</h3>
+            <p>
+              JavaSript is the language used to design the logic for the web
+            </p>
+          </div>
         </div>
       </div>
-
-      {/* <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div> */}
     </main>
   );
 }
