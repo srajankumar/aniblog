3dfeadc7de57da143f62465870981860058f8a29
srajankumar <kumarsrajan02@gmail.com>
2023-04-29 20:24:01 +0530

    blogs added, #19

--- src/pages/api/blogs.js
+++ src/pages/api/blogs.js
@@ -1,9 +1,28 @@
 //getting info from the json file and posting directly
 
+//http://localhost:3000/api/blogs
+
 import * as fs from "node:fs";
-export default function handler(req, res) {
-  fs.readdir("blogdata", (err, data) => {
-    console.log(data);
-    res.status(200).json(data);
-  });
+export default async function handler(req, res) {
+  let data = await fs.promises.readdir("blogdata");
+  let myfile;
+  let allBlogs = [];
+  for (let i = 0; i < data.length; i++) {
+    const item = data[i];
+    console.log(item);
+    myfile = await fs.promises.readFile("blogdata/" + item, "utf-8");
+    allBlogs.push(JSON.parse(myfile));
+  }
+  res.status(200).json(allBlogs);
+  // fs.promises.readdir("blogdata", (err, data) => {
+  //   console.log(data);
+  //   let allBlogs = {};
+  //   data.forEach((item) => {
+  //     console.log(item);
+  //     fs.readFile("blogdata/" + item, (d) => {
+  //       allBlogs.push(d);
+  //     });
+  //   });
+  //   res.status(200).json(allBlogs);
+  // });
 }
--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,28 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "../styles/Blog.module.css";
 import Link from "next/link";
 
 //Step 1: Collect all the files from blogdata directory
 //Step 2: Iterate through them end display them
 
+// http://localhost:3000/api/blogs
+
 const blog = () => {
+  const [blogs, setblogs] = useState([]);
+  useEffect(() => {
+    console.log("useeffect is running");
+    fetch("http://localhost:3000/api/blogs")
+      .then((a) => {
+        return a.json();
+      }) //fetches jsondata
+      .then((parsed) => {
+        console.log(parsed); //parse json file
+        setblogs(parsed);
+      });
+  }, []);
   return (
     <div className={styles.blogPage}>
       <h1 className="font-bold">Latest Blogs</h1>
-      <div className={styles.blogItem}>
-        <Link href={"/blogpost/learn-javascript"}>
-          <h2>How to learn HTML in 2023</h2>
-        </Link>
-        <p>JavaSript is the language used to design the logic for the web</p>
-      </div>
-      <div className="blogItem">
-        <h2>How to learn CSS in 2023</h2>
-        <p>JavaSript is the language used to design the logic for the web</p>
-      </div>
-      <div className="blogItem">
-        <h2>How to learn Javascript in 2023</h2>
-        <p>JavaSript is the language used to design the logic for the web</p>
-      </div>
+      {blogs.map((blogItem) => {
+        return (
+          <div className={styles.blogItem} key={blogItem.slug}>
+            <Link href={`/blogpost/${blogItem.slug}`}>
+              <h2>{blogItem.title}</h2>
+            </Link>
+            <p>{blogItem.content.substr(0, 200)}...</p>
+          </div>
+        );
+      })}
     </div>
   );
 };
