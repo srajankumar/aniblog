01d5cb105e8034ff81702e9f0cecdda6e7093503
srajankumar <kumarsrajan02@gmail.com>
2023-05-02 12:45:32 +0530

    26

---
+++ src/pages/contact.jsx
@@ -0,0 +1,122 @@
+import React, { useState } from "react";
+import styles from "../styles/Contact.module.css";
+
+const Contact = () => {
+  const [name, setname] = useState("");
+  const [email, setemail] = useState("");
+  const [phone, setphone] = useState("");
+  const [desc, setdesc] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(name, phone, email, desc);
+    async function postJSON(data) {
+      try {
+        const response = await fetch("http://localhost:3000/api/postcontact", {
+          method: "POST", // or 'PUT'
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(data),
+        })
+          .then((response) => response.text())
+          .then((data) => {
+            console.log("Success:", data);
+            alert("Thanks for contacting us");
+            setdesc("");
+            setemail("");
+            setname("");
+            setphone("");
+          });
+      } catch (error) {
+        console.error("Error:", error);
+      }
+    }
+
+    const data = { name, phone, email, desc };
+    postJSON(data);
+  };
+
+  const handleChange = (e) => {
+    if (e.target.name == "phone") {
+      setphone(e.target.value);
+    } else if (e.target.name == "email") {
+      setemail(e.target.value);
+    } else if (e.target.name == "desc") {
+      setdesc(e.target.value);
+    } else if (e.target.name == "name") {
+      setname(e.target.value);
+    }
+    // console.log(e, "change");
+  };
+  return (
+    <div className={styles.container}>
+      <h1>Contact us</h1>
+      <form onSubmit={handleSubmit}>
+        <div className={styles.mb3}>
+          <label for="name" className={styles.formlabel}>
+            Name
+          </label>
+          <input
+            value={name}
+            onChange={handleChange}
+            type="name"
+            className="form-control"
+            id="name"
+            name="name"
+            aria-describedby="emailHelp"
+          />
+        </div>
+
+        <div className={styles.mb3}>
+          <label for="email" className={styles.formlabel}>
+            Email
+          </label>
+          <input
+            value={email}
+            onChange={handleChange}
+            type="email"
+            className="form-control"
+            id="email"
+            name="email"
+            aria-describedby="emailHelp"
+          />
+        </div>
+
+        <div className={styles.mb3}>
+          <label for="phone" className={styles.formlabel}>
+            Phone
+          </label>
+          <input
+            value={phone}
+            onChange={handleChange}
+            type="phone"
+            className="form-control"
+            id="phone"
+            name="phone"
+            aria-describedby="emailHelp"
+          />
+        </div>
+
+        <div className={styles.mb3}>
+          <label for="description" className={styles.formlabel} id="phone">
+            Description
+          </label>
+          <textarea
+            value={desc}
+            onChange={handleChange}
+            className="form-control"
+            placeholder="write your consern"
+            id="desc"
+            name="desc"
+          />
+        </div>
+
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Contact;
--- src/pages/contact.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const contact = () => {
-  return <div>This is my contacs page</div>;
-};
-
-export default contact;
