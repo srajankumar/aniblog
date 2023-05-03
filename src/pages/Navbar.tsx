eea068a5eddd046d841f21cecb9b95ba817bff74
srajankumar <kumarsrajan02@gmail.com>
2023-05-03 15:38:45 +0530

    modified:   src/pages/Navbar.tsx modified:   src/pages/index.tsx

--- src/pages/Navbar.tsx
+++ src/pages/Navbar.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Link from "next/link";
-import styles from "../styles/Home.module.css";
+// import styles from "../styles/Home.module.css";
+const styles = require("../styles/Home.module.css");
 
 const Navbar = () => {
   return (
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import Script from "next/script";
-
-import styles from "../styles/Home.module.css";
+const styles = require("../styles/Home.module.css");
+// import styles from "../styles/Home.module.css";
 
 import { Inter } from "next/font/google";
 
