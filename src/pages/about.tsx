1b527bcdfd76c0505aa47a41915e5fccef86fde5
srajankumar <kumarsrajan02@gmail.com>
2023-05-07 11:02:58 +0530

    fixed issue

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -18,47 +18,48 @@ const about = () => {
         imaginations of millions of fans around the world. With its unique blend
         of stunning visuals, compelling stories, and dynamic characters, anime
         has become a cornerstone of modern entertainment. From the heartfelt
-        dramas to the action-packed adventures, there's something for everyone
-        to enjoy.
+        dramas to the action-packed adventures, there&apos;s something for
+        everyone to enjoy.
       </p>
       <h2>Services offered</h2>
       <p>
         Our blog is dedicated to showcasing some of the most popular and beloved
         anime series of all time, including both classic and contemporary
-        titles. Whether you're a long-time fan or a curious newcomer, we've got
-        you covered with reviews, analysis, and deep dives into the stories,
-        characters, and themes that make anime so captivating.
+        titles. Whether you&apos;re a long-time fan or a curious newcomer,
+        we&apos;ve got you covered with reviews, analysis, and deep dives into
+        the stories, characters, and themes that make anime so captivating.
       </p>
       <br />
       <p>
-        Here, you'll find blogs on some of the most popular anime series,
+        Here, you&apos;ll find blogs on some of the most popular anime series,
         including Naruto, Attack on Titan, One Piece, Demon Slayer, and many
-        more. We'll explore the epic battles, emotional moments, and
+        more. We&apos;ll explore the epic battles, emotional moments, and
         unforgettable characters that have made these series enduring favorites
         among fans.
       </p>
       <br />
       <p>
-        But we don't just stop at the classics - we also delve into newer series
-        that are sure to become future favorites. From the sci-fi adventures of
-        Steins;Gate to the supernatural thrills of Jujutsu Kaisen, we keep our
-        fingers on the pulse of the anime world and bring you the latest and
-        greatest shows.
+        But we don&apos;t just stop at the classics - we also delve into newer
+        series that are sure to become future favorites. From the sci-fi
+        adventures of Steins;Gate to the supernatural thrills of Jujutsu Kaisen,
+        we keep our fingers on the pulse of the anime world and bring you the
+        latest and greatest shows.
       </p>
       <br />
       <p>
         Through our blog, we aim to inspire a deeper appreciation for anime and
         help newcomers discover the joy and excitement of this incredible
         medium. We believe that anime has the power to entertain, educate, and
-        inspire, and we're passionate about sharing that message with the world.
+        inspire, and we&apos;re passionate about sharing that message with the
+        world.
       </p>
       <br />
       <p>
-        So, whether you're a seasoned otaku or just dipping your toes into the
-        anime waters, we invite you to join us on this amazing journey through
-        the world of anime. Get ready to immerse yourself in epic battles,
-        explore new worlds, and fall in love with unforgettable characters – all
-        through the magic of anime.
+        So, whether you&apos;re a seasoned otaku or just dipping your toes into
+        the anime waters, we invite you to join us on this amazing journey
+        through the world of anime. Get ready to immerse yourself in epic
+        battles, explore new worlds, and fall in love with unforgettable
+        characters – all through the magic of anime.
       </p>
       {/* <h2>Contact us</h2>
       <p>
